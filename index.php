ed54eb801acf9762b9a8a31b1f712392c793510c
Nguyen Van Hai <hainguyenvan6799>
2020-11-17 14:26:45 +0700

    update 17/11/2020

---
+++ index.php
@@ -0,0 +1,83 @@
+<?php 
+	require_once "./vendor/autoload.php";
+	$collection = (new MongoDB\Client)->mongodb->users;
+	// $insertOneResult = $collection->insertOne([
+	// 		"user_name" => "admin02",
+	// 		"email" => "cde@gmail.com",
+	// 		"password" => "admin",
+	// 		"favorite" => [
+	// 			"sport" => "swimming",
+	// 			"food" => "chicken"
+	// 		]
+	// 	]);
+
+	$document = $collection->findOne(
+		[
+			"user_name" => "admin02"
+		],
+		[
+			"projection" => ["_id" => 0]
+		]
+	);
+	// echo $document->favorite->sport;
+	// echo $document->_id;
+
+	// sort and limit
+	$cursor = $collection->find(["user_name" => "admin02"], [
+		"limit" => 2,
+		"sort" => ['email' => 1] // ['email' => -1] đối với -1 là giảm dần
+	]);
+
+	// regular expressions:
+	$regex = $collection->find([
+		'user_name' => ['$regex' => '^admin', '$options' => 'i']
+	]);
+
+	$aggregate = $collection->aggregate([
+		['$group' => ['_id'=> '$user_name', 'count' => ['$sum' => 1]]]
+	]);	
+
+	// updateMany, updateOne
+	// $update = $collection->updateMany(
+	// 	['user_name' => 'admin02'],
+	// 	['$set' => ['password' => 'admin123456']]
+	// );
+
+	// update with upserted option
+	// $replace = $collection->updateOne(
+	// 	['user_name' => 'hai_admin123456'],
+	// 	['$set' => ["email" => 'qakhudaubuon12@gmail.com']],
+	// 	['upsert' => true]
+	// );
+	// printf('Matched %d document', $replace->getMatchedCount());
+	// printf('Updated %d document', $replace->getModifiedCount());
+	// printf('Upserted %d document', $replace->getUpsertedCount());
+	// echo '<hr>';
+
+	$replace = $collection->replaceOne(
+		['user_name' => 'hai_admin123456'],
+		['email' => 'hainguyenvan6799@gmail.com', 'user_name' => 'hai_administrator']
+	);
+
+	printf("Matched %d document", $replace->getMatchedCount());
+	printf("Updated %d document", $replace->getModifiedCount());
+
+	echo '<hr>';
+
+	foreach($cursor as $c)
+	{
+		echo $c->user_name . '<br>';
+		echo $c->password . '<br>';
+	}
+	echo '<hr>';
+	foreach($regex as $r)
+	{
+		echo $r->user_name . '<br>';
+	}
+	echo '<hr>';
+	foreach($aggregate as $a)
+	{
+		echo $a["_id"] . '<br>';
+		echo $a["count"];
+	}
+ ?>
