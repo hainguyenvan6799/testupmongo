9019f61042634fc87901ef1b4e58b341c713b64e
Nguyen Van Hai <hainguyenvan6799>
2020-11-28 23:04:58 +0700

    update

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php 
 	require_once "./vendor/autoload.php";
 $client = new MongoDB\Client(
-    'mongodb+srv://hainguyenvan6799:Thu123456789@phongtro.ezstc.mongodb.net/mongodb?retryWrites=true&w=majority');
+    'mongodb+srv://hai:X2L3zGTavujQkwLW@phongtro.ezstc.mongodb.net/phongtrodb?retryWrites=true&w=majority');
 
 $db = $client->mongodb;
 	$userCollection = $db->users;
